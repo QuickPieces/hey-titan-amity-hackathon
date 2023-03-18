91b2867a841cb592317d256420397d277374b8be
bluebearrii <thananan.math.acs@gmail.com>
2023-03-19 01:37:15 +0700

    created chrome extension, in detecting search keyword on google website

--- chrome-extension/src/index.ts
+++ chrome-extension/src/index.ts
@@ -1,19 +1,17 @@
-chrome.tabs.onZoomChange.addListener((ZoomChangeInfo) => {
-  console.log(`Zoom level changed to ${ZoomChangeInfo.newZoomFactor}`);
+chrome.webNavigation.onCompleted.addListener((details) => {
+  console.log('GO', details);
 
-  const data = {
-    query: 'my search query string',
-  };
+  const url = details.url;
 
-  fetch('http://localhost:3000/api/search-receive', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      console.log('Sending search query to tracker service with status:', response.status);
-    })
-    .catch((error) => console.error('Error:', error));
+  console.log(url);
+
+  // Check if the URL is a Google search URL
+  if (url.includes('google.com/search')) {
+    // Get the search query parameters from the URL
+    const regex = /[\?&]q=([^&#]*)/;
+    const match = regex.exec(url);
+    const searchQuery = match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+
+    console.log("For Google", searchQuery);
+  }
 });
