bcf932835e6e3bb64da0434fdb26656c7a9d8623
bluebearrii <thananan.math.acs@gmail.com>
2023-03-19 01:43:42 +0700

    integrated with backend service

--- chrome-extension/src/index.ts
+++ chrome-extension/src/index.ts
@@ -3,8 +3,6 @@ chrome.webNavigation.onCompleted.addListener((details) => {
 
   const url = details.url;
 
-  console.log(url);
-
   // Check if the URL is a Google search URL
   if (url.includes('google.com/search')) {
     // Get the search query parameters from the URL
@@ -12,6 +10,22 @@ chrome.webNavigation.onCompleted.addListener((details) => {
     const match = regex.exec(url);
     const searchQuery = match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 
-    console.log("For Google", searchQuery);
+    console.log('For Google', searchQuery);
+
+    const data = {
+      query: searchQuery,
+    };
+
+    fetch('http://localhost:3000/api/search-receive', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        console.log('Sending search query to tracker service with status:', response.status);
+      })
+      .catch((error) => console.error('Error:', error));
   }
 });
